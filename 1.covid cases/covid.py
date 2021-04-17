c7365266247a7ca67e2e5778e283c5508045732e
Omkar Rajendra Lokhande <77824136+Omkar4141@users.noreply.github.com>
2021-04-17 09:04:25 +0530

    Update covid.py

--- 1.covid cases/covid.py
+++ 1.covid cases/covid.py
@@ -1,50 +1,65 @@
 from tkinter import *
 import tkinter.messagebox as t
-from  covid import  Covid
+
+from covid import Covid
+from plyer import notification
+
 import matplotlib.pyplot as p
-import time
+
+
 covid=Covid()
+
 
 root=Tk()
 
 root.geometry("800x500")
 
-root.title("covid-19->Project by :Omkar Lokhande and Viraj Raut")
+root.title("covid-19->Project by :Omkar Lokhande and Viraj Raut ")
+def notifyme(title,message):
+    notification.notify(
+        message=message,
+        timeout=50
 
+
+
+    )
 
 def showdata():
+    a = abc.get()
+    if (a == ""):
+        t.showinfo("info", "Please enter country name")
 
-    a=abc.get()
-    if(a==""):
-        t.showinfo("info","Please enter country name")
+    else:
+        try:
+            a = a.lower()
 
-    a=a.lower()
+            getdata = covid.get_status_by_country_name(a)
+
+            a = a.title()
+
+            p.title(f"Covid-19: Cases in {a}")
+            keys = ["confirmed cases", "recovered", "active cases", "death"]
+            values = [getdata["confirmed"], getdata["recovered"], getdata["active"], getdata["deaths"]]
+
+            p.axis("equal")
+            p.pie(values, labels=keys, radius=1, shadow=True, startangle=180, autopct="%0.0f%%")
+            p.legend()
+
+            p.text(1.727, -0.212, f"*Confirmed Cases:{values[0]}\n\n"
+                                  f"*Recovered Cases:{values[1]}\n\n"
+                                  f"*Active:{values[2]}\n\n"
+                                  f"*Death:{values[3]}")  # to add text on matplotlib
+            notifyme("GOVIND", "1.Stay Home Stay Safe ! \n"
+                               "2.Always Wear Mask on Face !\n"
+                               "3.Sanitize Your Hands ! \n"
+                               "4.Keep Social Distance !")
+
+            p.show()
 
 
-    getdata = covid.get_status_by_country_name(a)
+        except:
+            t.showinfo("info", "no such country data avaiable")
 
-
-
-    a=a.title()
-
-
-    p.title(f"Covid-19: Cases in {a}")
-    keys = ["confirmed cases","recovered" ,"active cases", "death"]
-    values = [getdata["confirmed"], getdata["recovered"], getdata["active"], getdata["deaths"]]
-
-    p.axis("equal")
-    p.pie(values, labels=keys, radius=1, shadow=True, startangle=180,autopct="%0.0f%%")
-    p.legend()
-
-    p.text(1.727,-0.212,f"*Confirmed Cases:{values[0]}\n\n"
-                        f"*Recovered Cases:{values[1]}\n\n"
-                        f"*Active:{values[2]}\n\n"
-                        f"*Death:{values[3]}")#to add text on matplotlib
-
-
-
-    p.show()
-    
 
 
 
@@ -64,15 +79,5 @@ screen.pack(padx=10,pady=10)
 print("\n")
 m=Button(root,bg="yellow",text="Show",padx=20,pady=5,font=("comicsansns",10,"bold"),command=showdata)
 m.pack()
-
-
-
-
-
-
-
-
-
-
 
 root.mainloop()
